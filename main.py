d1ee0269a3640427b7f5003be983db44ddec3922
AndersonRamírez <andersongamer037@gmail.com>
2023-12-12 19:08:48 -0500

    Git commands

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# Git diff - compara los cambio del último commit con tu código actual
+# Git reset --hard 
+# Git merge - traer los cambio de otra rama a la rama actual
+# Git stash - guardar los cambios del código pero sin hacer un commit
+
 print("Modifying the first commit for second time: Hello Git!")
